a5cd85f3f239f25595ee20ad54db108b8ae0f56d
Ryan McCullagh <me@ryanmccullagh.com>
2015-08-01 13:14:39 -0500

    c++ portability and win32 preprocessor check

--- smart_string.c
+++ smart_string.c
@@ -1,19 +1,19 @@
 /*
- *  Copyright (c) 2015 Ryan McCullagh <me@ryanmccullagh.com>
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+*  Copyright (c) 2015 Ryan McCullagh <me@ryanmccullagh.com>
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -24,16 +24,20 @@
 
 #define D_SMART_STRING_SIZE 16
 
-SMART_STRING_API 
-SmartString* smart_string_new(void)
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+SMART_STRING_API SmartString* smart_string_new(void)
 {
+
 	SmartString* ss;
-	if((ss = malloc(sizeof(SmartString))) == NULL) {
+	if ((ss = (SmartString* )malloc(sizeof(SmartString))) == NULL) {
 		return NULL;
 	}
 	ss->length = 0;
 	ss->capacity = D_SMART_STRING_SIZE;
-	if((ss->buffer = malloc(ss->capacity)) == NULL) {
+	if ((ss->buffer = (char *)malloc(ss->capacity)) == NULL) {
 		free(ss);
 		return NULL;
 	}
@@ -44,45 +48,45 @@ SmartString* smart_string_new(void)
 SMART_STRING_API
 bool SMART_STRING_FUNC(append, const char* str)
 {
-	if(ss == NULL) { return false; }
-	if(str == NULL) { return false; }
-	
+	if (ss == NULL) { return false; }
+	if (str == NULL) { return false; }
+
 	size_t part_length = strlen(str);
 	size_t i;
 	size_t current_position;
 	size_t new_length;
-	
-	if(part_length == 0) {
+
+	if (part_length == 0) {
 		return true;
 	}
-	
+
 	new_length = part_length + ss->length;
 	current_position = ss->length;
-	
-	if(new_length >= ss->capacity) {
-		while(new_length >= ss->capacity) {
+
+	if (new_length >= ss->capacity) {
+		while (new_length >= ss->capacity) {
 			ss->capacity *= 2;
-			if((ss->buffer = realloc(ss->buffer, ss->capacity)) == NULL) {
+			if ((ss->buffer = (char *)realloc(ss->buffer, ss->capacity)) == NULL) {
 				return false;
-			} 
+			}
 		}
 	}
-	
-	for(i = 0; i < part_length; i++) {
+
+	for (i = 0; i < part_length; i++) {
 		ss->buffer[current_position++] = str[i];
 	}
-	
+
 	ss->length = new_length;
 	ss->buffer[current_position] = '\0';
-	
+
 	return true;
 }
 
 SMART_STRING_API
 bool FORMAT_ATTR(2, 3) SMART_STRING_FUNC(append_sprintf, const char* format, ...)
 {
-	if((ss == NULL) || (format == NULL)) { return false; }
-	if(strlen(format) == 0) {
+	if ((ss == NULL) || (format == NULL)) { return false; }
+	if (strlen(format) == 0) {
 		return true;
 	}
 
@@ -90,21 +94,25 @@ bool FORMAT_ATTR(2, 3) SMART_STRING_FUNC(append_sprintf, const char* format, ...
 	int buffer_size = D_SMART_STRING_SIZE;
 	char* buffer, *buffer2;
 	int n;
-	if((buffer = malloc(buffer_size)) == NULL) {
+	if ((buffer = (char *)malloc(buffer_size)) == NULL) {
 		return false;
 	}
 
-	for(;;) {
+	for (;;) {
 		va_start(args, format);
-		n = vsnprintf(buffer, buffer_size, format, args);
+		#ifdef _WIN32
+				n = vsprintf_s(buffer, buffer_size, format, args);
+		#else
+				n = vsnprintf(buffer, buffer_size, format, args);
+		#endif
 		va_end(args);
 
-		if(n < 0) {
+		if (n < 0) {
 			free(buffer);
 			return false;
 		}
 
-		if(n < buffer_size) {
+		if (n < buffer_size) {
 			smart_string_append(ss, buffer);
 			free(buffer);
 			return true;
@@ -112,54 +120,56 @@ bool FORMAT_ATTR(2, 3) SMART_STRING_FUNC(append_sprintf, const char* format, ...
 
 		buffer_size = buffer_size + 1;
 
-		if((buffer2 = realloc(buffer, buffer_size)) == NULL) {
+		if ((buffer2 = (char *)realloc(buffer, buffer_size)) == NULL) {
 			free(buffer);
 			return false;
-		} else {
+		}
+		else {
 			buffer = buffer2;
 		}
 	}
 
-    return false;
+	return false;
+
 }
 
 SMART_STRING_API
 void smart_string_destroy(SmartString* ss)
 {
-	if(ss == NULL) {
+	if (ss == NULL) {
 		return;
 	}
 	#ifdef DEBUG
-		printf("debug:%s destroying SmartString instance: \n", __func__);
-		printf("SmartString {\n");
-		printf("\tlength   = %zu\n",  ss->length);
-		printf("\tcapacity = %zu\n",  ss->capacity);
-		if(ss->buffer != NULL) {
-			printf("\tbuffer   = \"%s\"\n", ss->buffer);
-		}
-		printf("}\n");
+	printf("%s(): destroying SmartString instance : \n", __func__);
+	printf("SmartString {\n");
+	printf("\tlength   = %zu\n", ss->length);
+	printf("\tcapacity = %zu\n", ss->capacity);
+	if (ss->buffer != NULL) {
+		printf("\tbuffer   = \"%s\"\n", ss->buffer);
+	}
+	printf("}\n");
 	#endif
-	if(ss->buffer != NULL) {
+	if (ss->buffer != NULL) {
 		free(ss->buffer);
 	}
-	
+
 	free(ss);
 }
 
 SMART_STRING_API
 bool SMART_STRING_FUNC(starts_with, const char* str)
 {
-	if((ss == NULL) || (str == NULL)) { return false; }
+	if ((ss == NULL) || (str == NULL)) { return false; }
 	size_t part_length = strlen(str);
 	size_t i;
-	if(part_length == 0) {
+	if (part_length == 0) {
 		return false;
 	}
-	if(part_length > ss->length) {
+	if (part_length > ss->length) {
 		return false;
 	}
-	for(i = 0; i < part_length; i++) {
-		if(ss->buffer[i] != str[i]) {
+	for (i = 0; i < part_length; i++) {
+		if (ss->buffer[i] != str[i]) {
 			return false;
 		}
 	}
@@ -170,21 +180,26 @@ bool SMART_STRING_FUNC(starts_with, const char* str)
 SMART_STRING_API
 bool SMART_STRING_FUNC(ends_with, const char* str)
 {
-	if((ss == NULL) || (str == NULL)) { return false; }
+	if ((ss == NULL) || (str == NULL)) { return false; }
 	int part_length = strlen(str);
 	int i;
-	if(part_length == 0) {
+	if (part_length == 0) {
 		return false;
 	}
-	if((unsigned int)part_length > ss->length) {
+	if ((unsigned int)part_length > ss->length) {
 		return false;
 	}
-	size_t end = ss->length - 1;	
-	for(i = (part_length -1); i >= 0; i--) {
-			if(ss->buffer[end--] != str[i]) {
-				return false;
-			}
+	size_t end = ss->length - 1;
+	for (i = (part_length - 1); i >= 0; i--) {
+		if (ss->buffer[end--] != str[i]) {
+			return false;
+		}
 	}
-	
+
 	return true;
 }
+
+#ifdef __cplusplus
+}
+#endif
+
--- smart_string.h
+++ smart_string.h
@@ -1,19 +1,19 @@
 /*
- *  Copyright (c) 2015 Ryan McCullagh <me@ryanmccullagh.com>
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+*  Copyright (c) 2015 Ryan McCullagh <me@ryanmccullagh.com>
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef __SMART_STRING_H__
 #define __SMART_STRING_H__
 
@@ -23,7 +23,11 @@
 #define SMART_STRING_FUNC(name, ...) \
 	smart_string_##name(SmartString* ss, __VA_ARGS__)
 
-#define FORMAT_ATTR(x, y) __attribute__ ((format (printf, x, y)))
+#if defined _WIN32
+	#define FORMAT_ATTR(x, y)
+#else
+	#define FORMAT_ATTR(x, y) __attribute__ ((format (printf, x, y)))
+#endif
 
 typedef struct SmartString {
 	size_t length;
@@ -31,42 +35,26 @@ typedef struct SmartString {
 	char* buffer;
 } SmartString;
 
-extern SmartString* smart_string_new(void);
-extern bool smart_string_append(SmartString* ss, const char* str);
-extern bool FORMAT_ATTR(2, 3) smart_string_append_sprintf(SmartString* ss, const char* format, ...);
-extern void smart_string_destroy(SmartString* ss);
-extern bool smart_string_starts_with(SmartString* ss, const char* str);
-extern bool smart_string_ends_with(SmartString* ss, const char* str);
 
-#if defined _WIN32 || defined __CYGWIN__
-  #ifdef BUILDING_DLL
-    #ifdef __GNUC__
-      #define SMART_STRING_PUBLIC __attribute__ ((dllexport))
-    #else
-      #define SMART_STRING_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
-    #endif
-  #else
-    #ifdef __GNUC__
-	#if __GNUC__ >= 4
-		#define SMART_STRING_PUBLIC __attribute ((visibility("default")))
-	#else
-      		#define SMART_STRING_PUBLIC __attribute__ ((dllimport))
-	#endif
-    #else
-      #define SMART_STRING_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
-    #endif
-  #endif
-  #define SMART_STRING_LOCAL
+#if defined _WIN32
+	#define SMART_STRING_PUBLIC __declspec(dllexport)
 #else
-  #if __GNUC__ >= 4
-    #define SMART_STRING_PUBLIC __attribute__ ((visibility ("default")))
-    #define SMART_STRING_LOCAL  __attribute__ ((visibility ("hidden")))
-  #else
-    #define SMART_STRING_PUBLIC
-    #define SMART_STRING_LOCAL
-  #endif
+	#define SMART_STRING_PUBLIC __attribute ((visibility("default")))
 #endif
 
 #define SMART_STRING_API SMART_STRING_PUBLIC
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	extern SMART_STRING_API SmartString* smart_string_new(void);
+	extern SMART_STRING_API bool smart_string_append(SmartString* ss, const char* str);
+	extern SMART_STRING_API bool FORMAT_ATTR(2, 3) smart_string_append_sprintf(SmartString* ss, const char* format, ...);
+	extern SMART_STRING_API void smart_string_destroy(SmartString* ss);
+	extern SMART_STRING_API bool smart_string_starts_with(SmartString* ss, const char* str);
+	extern SMART_STRING_API bool smart_string_ends_with(SmartString* ss, const char* str);
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __SMART_STRING_H__ */
